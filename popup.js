8d2ebd969dbd7eed51bee7f72f64a239e06f66dd
Samuel Stromswold <samstromsw@gmail.com>
2014-11-12 22:57:58 -0500

    Modified js to allow us to access functions, also did some mild arty things.

--- popup.js
+++ popup.js
@@ -1,16 +1,29 @@
 function maketbl(names){
   var str="<table>";
   for(i=0;i<names.length;i++){
-		str=str+"<tr><th onmouseover='highlight(this)' onmouseout='unhighlight(this)' onmouseclick='opentabs()'>"+names[i]+"</th></tr>";
+		str=str+"<tr><th id='names[i]>"+names[i]+"</th></tr>";
   }
   str=str+"</table>";
   document.getElementById("list").innerHTML=str;
+  
+  for(i=0;i<names.length;i++){
+  		item=document.getElementById(names[i]);
+		item.addEventListener("mouseover", function(){
+			item.backgroundColor="blue";
+		});
+		item.addEventListener("mouseout", function(){
+			item.backgroundColor="white";
+		});
+  }
 }
 
 function fetch(){
   var names=localStorage["names"];
+  names="asdf;fjfjf;uiuiuiu;xdxD";
   if(names==undefined){
     document.getElementById("list").innerHTML="<p>Use this extension to save your sessions!</p>";
+  }else{
+	maketbl(names.split(";"));  
   }
 }
 
@@ -22,8 +35,12 @@ function unhighlight(x){
   x.backgroundColor="white";
 }
 
+
+
 function opentabs(){}
 
-/*window.onload = function(){
-fetch();
-}*/
+function savetabs(){}
+
+window.addEventListener("load", fetch);
+
+document.getElementById("save").addEventListener("click", savetabs)
